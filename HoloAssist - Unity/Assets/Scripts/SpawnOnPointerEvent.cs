de336424285eccbbb4335f31011cdbd5579d9f0d
Sidharth Sudarsan <sidsarun@gmail.com>
2020-07-23 12:34:26 -0400

    add voice commands and instructions

--- HoloAssist - Unity/Assets/Scripts/SpawnOnPointerEvent.cs
+++ HoloAssist - Unity/Assets/Scripts/SpawnOnPointerEvent.cs
@@ -1,131 +1,103 @@
 ﻿using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.SpatialAwareness;
-using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
-
-
-// Example script that spawns a prefab at the pointer hit location.
-
 
 [AddComponentMenu("Scripts/MRTK/Examples/SpawnOnPointerEvent")]
 public class SpawnOnPointerEvent : MonoBehaviour
-    {
-        
+{
+    // Path, coordinate list, route display Renderer
+    private NavMeshPath path = null;
 
-        NavMeshAgent agent;
+    private Vector3[] positions = new Vector3[9];
 
+    public LineRenderer lr;
 
-        
-            // Path, coordinate list, route display Renderer 
-        NavMeshPath path = null;
-        Vector3[] positions = new Vector3[9];
-
-        public LineRenderer lr;
-
-        public GameObject cl;
-
-        public GameObject dest;
-
-        public GameObject parentObject;
+    public GameObject dest;
+    public TextToSpeech textToSpeech;
 
     public void Start()
-        {
-
-
-            
-            cl.SetActive(false);
-            lr.enabled = false;
-           
-    
-        }
-
-        public void Spawn(MixedRealityPointerEventData eventData)
-        {
-                var foundPosition = GetPositionOnSpatialMap();
-                Vector3 hitPos, hitNormal;
-                RaycastHit hitInfo;
-                Vector3 UiRayCastOrigin = Camera.main.transform.position;
-                Vector3 UiRayCastDirection = Camera.main.transform.forward;
-                //CoreServices.SpatialAwarenessSystem.SuspendObservers();
-                if (UnityEngine.Physics.Raycast(UiRayCastOrigin, UiRayCastDirection, out hitInfo))
-                {
-
-                    agent = cl.GetComponent<NavMeshAgent>();
-                    if (!cl.activeSelf)
-                    {
-                        cl.GetComponent<NavMeshAgent>().Warp(hitInfo.point);
-                        cl.SetActive(true);
-
-                    }
-                    else
-                    {
-                        cl.GetComponent<NavMeshAgent>().destination = dest.transform.position;
-                        lr.enabled = true;
-
-                        // path computation 
-                        path = new NavMeshPath();
-                        NavMesh.CalculatePath(cl.GetComponent<NavMeshAgent>().transform.position, cl.GetComponent<NavMeshAgent>().destination, NavMesh.AllAreas, path);
-                        positions = path.corners;
-
-                        // root drawing 
-                        lr.widthMultiplier = 0.2F;
-                        lr.positionCount = positions.Length;
-
-                        for (int i = 0; i < positions.Length; i++)
-                        {
-                            Debug.Log("point" + i + "=" + positions[i]);
-
-                            lr.SetPosition(i, positions[i]);
-
-                        }
-                        
-                    }
-
-                }
-                
-      
-
-        }
-
+    {
         
-        private static int _meshPhysicsLayer = 0;
-
-        private static int GetSpatialMeshMask()
+        lr.enabled = false;
+        textToSpeech.StartSpeaking("Welcome to HoloAssist! Say the destination out loud to see the path.");
+    }
+    private void Update()
+    {
+        if (Vector3.SqrMagnitude(Camera.main.transform.position - dest.transform.position) < 2.8)
         {
-            if (_meshPhysicsLayer == 0)
+
+            textToSpeech.StartSpeaking("You have arrived at your destination");
+        }
+    }
+
+    public async void OnStairs()
+    {
+        await Spawn();
+    }
+
+    async Task Spawn()
+    {
+        //RaycastHit hitInfo;
+        //Vector3 UiRayCastOrigin = Camera.main.transform.position;
+        //Vector3 UiRayCastDirection = Camera.main.transform.forward;
+
+        //if (UnityEngine.Physics.Raycast(UiRayCastOrigin, UiRayCastDirection, out hitInfo))
+        //{
+            // path computation
+            lr.enabled = true;
+            path = new NavMeshPath();
+            NavMesh.CalculatePath(new Vector3(Camera.main.transform.position.x, 0, Camera.main.transform.position.z), dest.transform.position, NavMesh.AllAreas, path);
+            positions = path.corners;
+
+            // root drawing
+            lr.widthMultiplier = 0.1F;
+            lr.positionCount = positions.Length;
+
+            for (int i = 0; i < positions.Length; i++)
             {
-                var spatialMappingConfig = CoreServices.SpatialAwarenessSystem.ConfigurationProfile as
-                    MixedRealitySpatialAwarenessSystemProfile;
-                if (spatialMappingConfig != null)
+                Debug.Log("point" + i + "=" + positions[i]);
+
+                lr.SetPosition(i, positions[i]);
+            }
+        //}
+    }
+
+    private static int _meshPhysicsLayer = 0;
+
+    private static int GetSpatialMeshMask()
+    {
+        if (_meshPhysicsLayer == 0)
+        {
+            var spatialMappingConfig = CoreServices.SpatialAwarenessSystem.ConfigurationProfile as
+                MixedRealitySpatialAwarenessSystemProfile;
+            if (spatialMappingConfig != null)
+            {
+                foreach (var config in spatialMappingConfig.ObserverConfigurations)
                 {
-                    foreach (var config in spatialMappingConfig.ObserverConfigurations)
+                    var observerProfile = config.ObserverProfile
+                        as MixedRealitySpatialAwarenessMeshObserverProfile;
+                    if (observerProfile != null)
                     {
-                        var observerProfile = config.ObserverProfile
-                            as MixedRealitySpatialAwarenessMeshObserverProfile;
-                        if (observerProfile != null)
-                        {
-                            _meshPhysicsLayer |= (1 << observerProfile.MeshPhysicsLayer);
-                        }
-                        
+                        _meshPhysicsLayer |= (1 << observerProfile.MeshPhysicsLayer);
                     }
                 }
             }
-
-            return _meshPhysicsLayer;
         }
-        public static Vector3? GetPositionOnSpatialMap(float maxDistance = 2)
+
+        return _meshPhysicsLayer;
+    }
+
+    public static Vector3? GetPositionOnSpatialMap(float maxDistance = 2)
+    {
+        RaycastHit hitInfo;
+        var transform = Camera.main.transform;
+        if (UnityEngine.Physics.Raycast(transform.position, transform.forward, out hitInfo, maxDistance, GetSpatialMeshMask()))
         {
-            RaycastHit hitInfo;
-            var transform = Camera.main.transform;
-            if (UnityEngine.Physics.Raycast(transform.position, transform.forward, out hitInfo, maxDistance, GetSpatialMeshMask()))
-            {
-                return hitInfo.point;
-            }
-            return null;
+            return hitInfo.point;
         }
-
-
+        return null;
+    }
 }
